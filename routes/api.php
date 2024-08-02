51dc282a21c9d0955f26abf2bbfc6c5ad887066d
StefanoAntonelli93 <ste.antonelli@outlook.it>
2024-08-02 13:36:08 +0200

    feat: add route api adn ProjectController in Controller->api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\api\ProjectController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// richiamo controller Project
+// in rpute api c'è sempre prefix -> api
+Route::get('projects', [ProjectController::class, 'index']);
